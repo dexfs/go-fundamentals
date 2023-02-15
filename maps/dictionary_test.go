7b62328cd443eeb86cfe3361cab9564fa5a792bb
Andre Santos <andre@plugpagamentos.com>
2023-02-14 21:41:27 -0300

    feat: maps - part-1

---
+++ maps/dictionary_test.go
@@ -0,0 +1,52 @@
+package maps
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	//dictionary := map[string]string{"test": "this is just a test"}
+
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("Known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+		assertError(t, got, ErrNotFound)
+	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertStrings(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
+	}
+}
