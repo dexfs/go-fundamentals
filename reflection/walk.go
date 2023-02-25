80ef588b6216a17d777f0303a6ea6947438f2321
Andre Santos <andre@plugpagamentos.com>
2023-02-25 15:42:31 -0300

    feat: reflection v4

--- reflection/walk.go
+++ reflection/walk.go
@@ -4,23 +4,23 @@ import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
+	numberOfValues := 0
 
-	if val.Kind() == reflect.Slice {
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
-		return
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice:
+		numberOfValues = val.Len()
+		getField = val.Index
 	}
 
-	for i := 0; i < val.NumField(); i++ {
-		field := val.Field(i)
-
-		switch field.Kind() {
-		case reflect.String:
-			fn(field.String())
-		case reflect.Struct:
-			walk(field.Interface(), fn)
-		}
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
 	}
 }
 
