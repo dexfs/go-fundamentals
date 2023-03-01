8c98fadf691cb0a48a12752a42fd500490f09d9f
Andre Santos <andre@plugpagamentos.com>
2023-02-28 21:52:29 -0300

    feat: roman numerals v2

--- roman-numerals/numeral.go
+++ roman-numerals/numeral.go
@@ -1,11 +1,21 @@
 package roman_numerals
 
+import "strings"
+
 func ConvertToRoman(arabic int) string {
-	if arabic == 3 {
-		return "III"
+	var result strings.Builder
+
+	for i := arabic; i > 0; i-- {
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
+		result.WriteString("I")
 	}
-	if arabic == 2 {
-		return "II"
-	}
-	return "I"
+
+	return result.String()
 }
--- roman-numerals/numeral_test.go
+++ roman-numerals/numeral_test.go
@@ -11,7 +11,10 @@ func TestRomanNumerals(t *testing.T) {
 		{"1 gets converted to I", 1, "I"},
 		{"2 gets converted to II", 2, "II"},
 		{"3 gets converted to III", 3, "III"},
+		{"4 gets converted to IV (can't repeat more than 3 times)", 4, "IV"},
+		{"5 gets converted to V", 5, "V"},
 	}
+
 	for _, test := range cases {
 		t.Run(test.Description, func(t *testing.T) {
 			got := ConvertToRoman(test.Arabic)
