bbb4a2924b51623662aba9ecb5eebd0a3ed2f506
Andre Santos <andre@plugpagamentos.com>
2023-03-13 18:27:39 -0300

    feat: add template improvements

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -6,23 +6,31 @@ import (
 	"io"
 )
 
-type Post struct {
-	Title, Body, Description string
-	Tags                     []string
-}
-
 var (
 	//go:embed "templates/*"
 	postTemplate embed.FS
 )
 
-func Render(w io.Writer, p Post) error {
-	templ, err := template.ParseFS(postTemplate, "templates/*.gohtml")
+type Post struct {
+	Title, Body, Description string
+	Tags                     []string
+}
+
+type PostRenderer struct {
+	templ *template.Template
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	tmpl, err := template.ParseFS(postTemplate, "templates/*.gohtml")
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	if err := templ.Execute(w, p); err != nil {
+	return &PostRenderer{templ: tmpl}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	if err := r.templ.Execute(w, p); err != nil {
 		return err
 	}
 
