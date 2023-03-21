d757130a079562438854f5079190121d6a153ed0
Andre Santos <andre@plugpagamentos.com>
2023-03-20 21:57:33 -0300

    feat: generics with arrays and slice - v2

--- array-and-slices/bad_bank.go
+++ array-and-slices/bad_bank.go
@@ -6,7 +6,43 @@ type Transaction struct {
 	Sum  float64
 }
 
-func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	}
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
+	}
+
+	return a
+}
+
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
+}
+func Reduce[A, B any](collection []A, accumulator func(B, A) B, initialValue B) B {
 	var result = initialValue
 	for _, x := range collection {
 		result = accumulator(result, x)
@@ -16,14 +52,14 @@ func Reduce[A any](collection []A, accumulator func(A, A) A, initialValue A) A {
 }
 
 func BalanceFor(transactions []Transaction, name string) float64 {
-	var balance float64
-	for _, t := range transactions {
+	adjustBalance := func(currentBalance float64, t Transaction) float64 {
 		if t.From == name {
-			balance -= t.Sum
+			return currentBalance - t.Sum
 		}
 		if t.To == name {
-			balance += t.Sum
+			return currentBalance + t.Sum
 		}
+		return currentBalance
 	}
-	return balance
+	return Reduce(transactions, adjustBalance, 0.0)
 }
