fda60d394cf908a841b1e128b7e437c2a3d18d7c
Andre Santos <andre@plugpagamentos.com>
2023-03-28 22:22:23 -0300

    feat: http-server - v1

---
+++ http-server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type PlayerScore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerScore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	score := p.store.GetPlayerScore(player)
+
+	if score == 0 {
+		w.WriteHeader(http.StatusNotFound)
+	}
+
+	fmt.Fprint(w, score)
+}
+
+func GetPlayerScore(name string) int {
+	if name == "Pepper" {
+		return 20
+	}
+
+	if name == "Floyd" {
+		return 10
+	}
+
+	return 0
+}
