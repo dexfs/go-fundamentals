0a538c70287775dd9c650e36045bc1eb9a414852
Andre Santos <andre@plugpagamentos.com>
2023-04-09 17:34:46 -0300

    feat(CLI): create CLI and tests - v1

---
+++ http-server/CLI.go
@@ -0,0 +1,22 @@
+package poker
+
+import (
+	"bufio"
+	"io"
+	"strings"
+)
+
+type CLI struct {
+	playerStore PlayerStore
+	in          io.Reader
+}
+
+func (cli *CLI) PlayPoker() {
+	reader := bufio.NewScanner(cli.in)
+	reader.Scan()
+	cli.playerStore.RecordWin(extractWinner(reader.Text()))
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
+}
